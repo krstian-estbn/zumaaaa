3785071270bddd5d4deba052457ce70a40c0be47
tebs <kristianesteban0213@gmail.com>
2026-06-02 16:45:09 +0800

    fixed leaderboards issue with same name

--- model/leaderboard.py
+++ model/leaderboard.py
@@ -35,18 +35,31 @@ def save_score(mode: Mode, name: str, exp: int, rounds: int) -> None:
     leaderboard: Dict[str, List[LeaderboardEntry]] = load_leaderboard()
     mode_key = mode.name.lower()
     
-    leaderboard[mode_key].append({
-        "name": name,
-        "exp": exp,
-        "rounds": rounds
-    })
+    entries: List[LeaderboardEntry] = leaderboard[mode_key]
+    existing_entry: None | LeaderboardEntry = None
     
-    leaderboard[mode_key].sort(
-        key=lambda x: (x["rounds"], x["exp"]),
-        reverse=True        
-        )
+    for entry in entries:
+        if entry["name"].lower() == name.lower():
+            existing_entry = entry
+            break
     
-    leaderboard[mode_key] = leaderboard[mode_key][:10] # keep 10 lang muna
+    if existing_entry:
+        better_rounds: bool = rounds > existing_entry["rounds"]
+        better_exp: bool = (rounds == existing_entry["rounds"] and exp > existing_entry["exp"])
+        
+        if better_rounds or better_exp:
+            existing_entry["rounds"] = rounds
+            existing_entry["exp"] = exp
+    else:
+        entries.append({
+            "name": name,
+            "exp": exp,
+            "rounds": rounds
+        })
+    
+
+    entries.sort(key=lambda x: (x["rounds"], x["exp"]), reverse=True)
+    leaderboard[mode_key] = entries[:10] # keep 10 lang muna
     
     with open(LEADERBOARD, "w", encoding="utf-8") as file:
         json.dump(leaderboard, file, indent=4)
