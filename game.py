5fa01e54974cd4408b88dacc6959e1a6f8db68e3
aidrnnn <apadriano@up.edu.ph>
2026-06-02 20:55:31 +0800

    main.py -> game.py; based it sa instructions

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from controller import Controller
 import pyxel
 
 if __name__ == '__main__':
-    pyxel.init(208, 208, title="Zuma: Tower Defense")
+    pyxel.init(208, 208, title="Zuma: Tower Defense Game")
     model = Model()  # logic
     view = View()  # what the user sees
     controller = Controller(model, view)  # glues model & view
